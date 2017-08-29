bb632e2c59c52b77d91e5ddc272f6bf160036054
vgimeno <victorgimeno.java@gmail.com>
2017-08-29 21:20:37 +0200

    modificado el cotenedor s3

--- script_arranque_httpd_copia.sh
+++ script_arranque_httpd_copia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install httpd -y
 yum update -y
-aws s3 cp s3://cap34.acloudguru.autoweb/index.html /var/www/html
+aws s3 cp s3://vgimeno-acloud-resources/html/* /var/www/html
 service httpd start
 chkconfig httpd on
--- script_arranque_php_db.sh
+++ script_arranque_php_db.sh
@@ -4,7 +4,5 @@ yum update -y
 chkconfig httpd on
 service httpd start
 echo "<?php phpinfo(); ?>" > /var/www/html/index.php
-cd /var/www/html
-
-
-aws s3 cp s3://cap34.acloudguru.autoweb/index.html /var/www/html
+# cd /var/www/html
+aws s3 cp s3://vgimeno-acloud-resources/php/connect.php /var/www/html
